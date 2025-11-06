ed3d87a65f6bf49744ce416337986f9c55de572f
root <root@ip-172-31-10-133.ap-south-1.compute.internal>
2025-11-06 08:05:21 +0000

    Test CI/CD pipeline: updated message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from Flask CI/CD Demo (by Arjun23-jpg)"
+    return "Flask CI/CD is Working Successfully 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
